678ff1459b15cad291aa643b91d2d066ac809e74
keatonBraith <keeters33@live.com>
2020-08-24 16:57:54 -0600

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = ({totalCounters}) => {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+
+
+export default NavBar;
